0ecd53c0e84491c9827c7da9fb385ce70a21e15e
jdanas <59902137+jdanas@users.noreply.github.com>
2023-10-11 12:17:12 +0800

    updated note list function

--- src/components/NoteList/NoteList.js
+++ src/components/NoteList/NoteList.js
@@ -4,11 +4,21 @@ import { NotesContext } from "../../provider/NotesProvider";
 import { fetchAction } from "../../reducer/notes";
 
 // this component can be used to list out all existing notes and make them clickable to be shown in the noteDetail compenents
+
 export default function NoteList({modeChange, noteChange}) {
   const { NotesDispatch, notesList } = useContext(NotesContext);
 
   return (
     <div className="listContainer">
+      <h2 className="notesHeader">Notes</h2>
+      <ul className="notesList">
+        {notesList.map((note) => (
+          <li key={note.id} onClick={() => noteChange(note)} >
+            {note.title}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
+
