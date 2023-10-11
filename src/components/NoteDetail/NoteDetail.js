558b5a528d05a035855cfa65d46c12f284672fea
Ye Maw <sawyemaw@sph.com.sg>
2023-10-11 12:06:38 +0800

    Update NoteDetail.js

--- src/components/NoteDetail/NoteDetail.js
+++ src/components/NoteDetail/NoteDetail.js
@@ -7,10 +7,30 @@ import { addAction, deleteAction, editAction } from "../../reducer/notes";
 export default function NoteDetail(PROPS) {
   const { NotesDispatch, notesList } = useContext(NotesContext);
 
- 
-
   return (
-    <div className="detailContainer">
+    <div class="detailContainer">
+      <div class="detailHeader">
+        <div class="titleContainer">
+          <input
+            class="titleInput"
+            type="text"
+            placeholder="Enter Note Title Here!"
+            size="50"
+            value={PROPS.title}
+          />
+        </div>
+        <div class="dateContainer">
+          <p class="dateDisplay"></p>
+        </div>
+      </div>
+      <div class="noteDetails">
+        <div class="noteContent">
+          <textarea placeholder="Enter your Note Here!" class="contentInput">
+            {PROPS.content}
+          </textarea>
+        </div>
+        <button class="editBtn">Save</button>
+      </div>
     </div>
   );
 }
